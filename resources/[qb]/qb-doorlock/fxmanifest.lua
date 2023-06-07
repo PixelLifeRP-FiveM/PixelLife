813e41e659dadc89b8cb11aa774d14affbff53d0
PixelLife-DevUpdates <khaikenji8@gmail.com>
2023-06-07 17:01:10 -0500

    Changed Apartment to one and new spawn

--- resources/[qb]/qb-doorlock/fxmanifest.lua
+++ resources/[qb]/qb-doorlock/fxmanifest.lua
@@ -1,26 +1,18 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'Doorlock system for the QBCore Framework'
-version '2.0.0'
-
-ui_page 'html/index.html'
+lua54 'yes'
 
 shared_scripts {
-    'config.lua',
-    'configs/*.lua',
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua' -- Change this to your preferred language
+    'shared/sh_*.lua',
 }
 
-server_script 'server/main.lua'
-client_script 'client/main.lua'
-
-files {
-	'html/*.html',
-	'html/*.js',
-	'html/*.css',
-	'html/sounds/*.ogg',
+client_script {
+    '@mercy-assets/client/cl_errorlog.lua',
+    'client/cl_*.lua',
 }
 
-lua54 'yes'
+server_script {
+    '@mercy-assets/server/sv_errorlog.lua',
+    'server/sv_*.lua',
+}
